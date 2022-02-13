e32fb092e9155192411ef54b41c8a18bd8eba355
SaraAbouelela <84548573+SaraAbouelela@users.noreply.github.com>
2022-02-14 01:54:49 +0200

    Adding the project skeleton and creating the PlaceHolderConfiguration API file

---
+++ PlaceHolder/src/main/java/placeHolder/framework/PlaceHolderConfiguration.java
@@ -0,0 +1,11 @@
+package placeHolder.framework;
+
+import io.restassured.RestAssured;
+import org.testng.annotations.BeforeMethod;
+
+public class PlaceHolderConfiguration{
+	@BeforeMethod(alwaysRun = true)
+	public void configure() {
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+	}
+}
